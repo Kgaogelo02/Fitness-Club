18bfa3faf477cd002ed486fc8fd929d0888bbe84
Mabutsi Kgaogelo Kgagara <112955221+Kgaogelo02@users.noreply.github.com>
2026-03-24 21:30:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
-    role = db.Column(db.String(50), default="member")      # user role: member/admin
+    role = db.Column(db.String(50), default="member")
 
 class Trainer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
